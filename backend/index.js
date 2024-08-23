e625b3e515080f437cb3479497e0a0131eaf0d2d
Varun-MSI <tvarun2014@gmail.com>
2024-08-23 12:03:12 +0530

    New

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
